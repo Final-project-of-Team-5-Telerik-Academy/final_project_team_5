92627d2a14cafae63874c3740d0a7fe0f7189b78
Yasen Sotirov <ia.sotirov@gmail.com>
2023-11-12 11:57:01 +0200

    Add unfinished endpoints for Match. Need additional validations and functionalities from Users

---
+++ my_models/model_match.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, constr
+from datetime import date
+
+
+class Match(BaseModel):
+    # FOR CREATING MATCH
+    id: int | None
+    date: date
+    player_1: str   # TODO Player
+    player_2: str   # TODO Player
+    title: str
+    format: constr(pattern='^knockout|league$')
+    prize: int
+    is_part_of_a_tournament: bool
+    creator: str    # TODO: connect with user
+
+    @classmethod
+    def from_query_result(cls, id, date, player_1, player_2, title, format, prize, is_part_of_a_tournament ):
+        return cls(id = id,
+                   date = date,
+                   player_1 = player_1,
+                   player_2 = player_2,
+                   title = title,
+                   format = format,
+                   prize = prize,
+                   is_part_of_a_tournament = is_part_of_a_tournament)
+
---
+++ services/match_service.py
@@ -0,0 +1,56 @@
+from data.database import insert_query, read_query
+from my_models.model_match import Match
+
+
+def get_all_matches():
+    data = read_query(f'''SELECT id, title, player_1, player_2, date, format, prize, is_part_of_a_tournament
+             FROM match_score_db.matches"''')
+    result = (Match.from_query_result(*row) for row in data)
+    return result
+
+
+def get_match_by_title(title: str):
+    data = read_query(f'''SELECT id, title, player_1, player_2, date, format, prize, is_part_of_a_tournament
+             FROM match_score_db.matches 
+             WHERE title LIKE "%{title}%"''')
+    result = (Match.from_query_result(*row) for row in data)
+    return result
+
+
+
+def get_sorted_matches(sort: str):
+    if sort == 'ascending':
+        sort = 'ASC'
+    else:
+        sort = "DESC"
+
+    data = read_query(f'''SELECT id, title, player_1, player_2, date, format, prize, is_part_of_a_tournament
+        FROM match_score_db.matches ORDERED BY title {sort}''')
+    result = (Match.from_query_result(*row) for row in data)
+    return result
+
+
+# TODO: connect with real players
+def create_match(date: str,
+                 title: str,
+                 player_1: str,
+                 player_2: str,
+                 match_format: str,
+                 prize: int,
+                 creator: str,
+                 tournament_name: str | None = None):
+    generated_match = insert_query(
+        '''INSERT INTO matches (title, player_1, player_2, date, format, prize, tournament_name, users_creator_id
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+        (date, title, player_1, player_2, match_format, prize, creator, tournament_name))
+
+    result = Match(id=generated_match,
+                   date = date,
+                   title = title,
+                   player_1 = player_1,
+                   player_2 = player_2,
+                   match_format = match_format,
+                   prize = prize,
+                   tournament_name = tournament_name,
+                   creator = creator)
+    return result
