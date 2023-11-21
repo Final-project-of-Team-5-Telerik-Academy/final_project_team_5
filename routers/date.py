970fef969d4887f8a843f5e49f985c484e385e8b
Yasen Sotirov <ia.sotirov@gmail.com>
2023-11-21 16:47:57 +0200

    add sort functionality to statistics

--- routers/date.py
+++ routers/date.py
@@ -18,22 +18,22 @@ def check_current_date():
 
 "SET SPECIFIC DATE IN THE FUTURE"
 @date_router.post('/date/{future_date}', description='Set specific date in the future in format "yyyy-mm-dd" (2023-3-15).')
-def set_specific_date(date: str):
+def set_specific_date(new_date: str):
 # check format is correct    alternative: ^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$
     pattern = re.compile(r'^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$')
-    if not pattern.match(date):
+    if not pattern.match(new_date):
         return JSONResponse(status_code=400, content="The date must be in format 'yyyy-mm-dd' (2023-3-15)")
 
 # check if date is not in past
-    if not date_service.date_is_in_future(date):
+    if not date_service.date_is_in_future(new_date):
         return JSONResponse(status_code=400, content=f'Today is {date_service.current_date()}, please set a date in the future')
 
 # play upcoming matches before this date
-    format_date = datetime.strptime(date, "%Y-%m-%d").date()
-    match_service.play_match(format_date)
+    new_date = datetime.strptime(new_date, "%Y-%m-%d").date()
+    match_service.play_match(new_date)
 
 # set the new date
-    result = date_service.add_date(format_date)
+    result = date_service.add_date(new_date)
     return result
 
 
